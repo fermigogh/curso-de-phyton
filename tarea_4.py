8c08d74e442859e6a4e10cce0b2e552c82f078ce
fermigogh <fernandomirandaesj@gmail.com>
2026-02-13 16:07:33 -0700

    replanteo del trabajo

--- tarea_4.py
+++ tarea_4.py
@@ -1,10 +1,11 @@
-nombre =[""]
-apellido =[""]
-email =[""]
-edad =[]
-
-
-
+lista_nombre =[""]
+lista_apellido =[""]
+lista_email =[""]
+lista_edad =[]
+nombre : str
+apellido : str
+email : str
+edad : int
 x = 1
 
 usuario_NR = int
@@ -14,69 +15,69 @@ while  x == 1:
     
     while True:
         
-            nombre.append(input("Por favor, ingresa tu nombre: "))
-            if len(nombre[-1]) < 3:
+            nombre(input("Por favor, ingresa tu nombre: "))
+            if len(nombre) < 3:
                 print("DENEGADO: El nombre debe tener al menos 3 caracteres.")
             
                 # Validación: verificar si hay números en la cadena
-            elif any(char.isdigit() for char in nombre[-1]):
+            elif any(char.isdigit() for char in nombre):
                     # Generar error si incluye números
                     print("DENEGADO: El nombre o el apellido no debe contener números.")
                 
-            elif not nombre[-1].isalpha():
+            elif not nombre.isalpha():
                     print("DENEGADO: El nombre o el apellido no debe contener caracteres especiales.")
             else:# Si no hay números, el nombre es válido
-                    print(f"VALIDADO: {nombre[-1]}")
+                    print(f"VALIDADO: {nombre}")
                     break
     while True:
         
             apellido.append(input("Por favor, ingresa tu apellido: "))
-            if len(apellido[-1]) < 3:
+            if len(apellido) < 3:
                 print("DENEGADO: El apellido debe tener al menos 3 caracteres.")
                 # Validación: verificar si hay números en la cadena
-            elif any(char.isdigit() for char in apellido[-1]):
+            elif any(char.isdigit() for char in apellido):
                     # Generar error si incluye números
                     print("DENEGADO: El apellido no debe contener números.")
                 
-            elif not apellido[-1].isalpha():
+            elif not apellido.isalpha():
                     print("DENEGADO: El apellido no debe contener caracteres especiales.")
             else:# Si no hay números, el nombre es válido
-                    print(f"VALIDADO: {apellido[-1]}") # apellido[-1] accede al último apellido agregado
+                    print(f"VALIDADO: {apellido}") # apellido[-1] accede al último apellido agregado
                     break
     while True:
         
             edad.append(input("Por favor, ingresa tu edad: "))
                 
                     # Validación: verificar si hay números en la cadena
-            if not edad[-1].isdigit():
+            if not edad.isdigit():
                         # Generar error si incluye números
                     print(f"DENEGADO: aprende que solo son numeros en edad")
-            elif int(edad[-1]) <=0 or int(edad[-1]) >=120:
+            elif int(edad) <=0 or int(edad) >=120:
                     print("Error: Edad no valida. Ingresa una edad entre 0 y 120.")
                        
-            elif int(edad[-1]) < 18:
-                    diferencia = 18 - int(edad[-1])
+            elif int(edad) < 18:
+                    diferencia = 18 - int(edad)
                     print(f"Error: Debes ser mayor de edad para registrarte. te faltan {diferencia} años")
-            elif int(edad[-1]) == 18:
-                    print(f"VALIDADO: {edad[-1]} apenas llegaste a la mayoria de edad")
+            elif int(edad) == 18:
+                    print(f"VALIDADO: {edad} apenas llegaste a la mayoria de edad")
                     break
             else:# Si no hay letras, la edad  es válido
-                    diferencia= int(edad[-1]) - 18 
-                    print(f"VALIDADO: {edad[-1]} eres mayor de edad superaste la mayoria de edad por {diferencia} años")
+                    diferencia= int(edad) - 18 
+                    print(f"VALIDADO: {edad} eres mayor de edad superaste la mayoria de edad por {diferencia} años")
                     break
     while True:
         
             email.append(input("Por favor, ingresa tu email (gmail, hotmail, yahoo): "))
 
                 # verificar si hay '@' en la cadena
-            if "@" not in email[-1] or "." not in email[-1]:
+            if "@" not in email or "." not in email:
                     #error si no incluye '@'
                     print("DENEGADO: El email debe contener '@' y un dominio válido.")
                 
             else:# Si incluye '@', el email es válido
                     
-                    if email[-1].count('@') == 1 and email[-1].endswith(('.com', '.com.mx', '.mx')):
-                        partes = email[-1].split('@')
+                    if email.count('@') == 1 and email.endswith(('.com', '.com.mx', '.mx')):
+                        partes = email.split('@')
                         antes_arroba = partes[0]
                         despues_arroba = partes[1]
                         if len(antes_arroba) > 2 and any(despues_arroba.startswith(dominio) for dominio in ['gmail', 'hotmail', 'yahoo']):
@@ -89,6 +90,7 @@ while  x == 1:
                         
 
     x=int(input("DESEA INSERTAR DATOS? 1 PARA SI, 2 PARA NO"))
+    
 
 while True:
     val_verusuario = input("DESEA VER USUARIOS? SI o NO ")
