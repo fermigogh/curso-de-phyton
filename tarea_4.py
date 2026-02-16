eb6dc16a2c4b1d7daea6b153179456810b50d1e2
fermigogh <fernandomirandaesj@gmail.com>
2026-02-16 15:02:05 -0700

    nuevas listas y primer testeo

--- tarea_4.py
+++ tarea_4.py
@@ -1,21 +1,20 @@
-lista_nombre =[""]
-lista_apellido =[""]
-lista_email =[""]
-lista_edad =[]
-nombre : str
-apellido : str
-email : str
-edad : int
+nombre: str
+apellido: str
+email: str
+edad: int
+lista_nombre =["muestra", ]
+lista_apellido =["muestra", ]
+lista_email =["correodemuestra_gmail.com", ]
+lista_edad =[18, ]
 x = 1
 
-usuario_NR = int
 
 x=int(input("DESEA INSERTAR DATOS? 1 PARA SI, 2 PARA NO: "))
 while  x == 1:
     
     while True:
         
-            nombre(input("Por favor, ingresa tu nombre: "))
+            nombre = input("Por favor, ingresa tu nombre: ")
             if len(nombre) < 3:
                 print("DENEGADO: El nombre debe tener al menos 3 caracteres.")
             
@@ -24,14 +23,17 @@ while  x == 1:
                     # Generar error si incluye números
                     print("DENEGADO: El nombre o el apellido no debe contener números.")
                 
-            elif not nombre.isalpha():
+            elif nombre.isalpha() == False:
                     print("DENEGADO: El nombre o el apellido no debe contener caracteres especiales.")
             else:# Si no hay números, el nombre es válido
                     print(f"VALIDADO: {nombre}")
+                    nombre = nombre.lower()  # Convertir a minúsculas
+                    nombre = nombre.capitalize()  # Convertir la primera letra a mayúscula
+                    lista_nombre.append(nombre)
                     break
     while True:
         
-            apellido.append(input("Por favor, ingresa tu apellido: "))
+            apellido = input("Por favor, ingresa tu apellido: ")
             if len(apellido) < 3:
                 print("DENEGADO: El apellido debe tener al menos 3 caracteres.")
                 # Validación: verificar si hay números en la cadena
@@ -39,35 +41,40 @@ while  x == 1:
                     # Generar error si incluye números
                     print("DENEGADO: El apellido no debe contener números.")
                 
-            elif not apellido.isalpha():
+            elif apellido.isalpha() == False:
                     print("DENEGADO: El apellido no debe contener caracteres especiales.")
             else:# Si no hay números, el nombre es válido
-                    print(f"VALIDADO: {apellido}") # apellido[-1] accede al último apellido agregado
+                    print(f"VALIDADO: {apellido}")
+                    apellido = apellido.lower()  # Convertir a minúsculas 
+                    apellido = apellido.capitalize()  # Convertir la primera letra a mayúscula
+                    lista_apellido.append(apellido)
                     break
     while True:
-        
-            edad.append(input("Por favor, ingresa tu edad: "))
+       
+           
+                edad =input("Por favor, ingresa tu edad: ")
                 
                     # Validación: verificar si hay números en la cadena
-            if not edad.isdigit():
+                if not edad.isdigit():
                         # Generar error si incluye números
-                    print(f"DENEGADO: aprende que solo son numeros en edad")
-            elif int(edad) <=0 or int(edad) >=120:
-                    print("Error: Edad no valida. Ingresa una edad entre 0 y 120.")
+                        print(f"DENEGADO: aprende que solo son numeros en edad")
+                elif int(edad) <=0 or int(edad) >=120:
+                        print("Error: Edad no valida. Ingresa una edad entre 0 y 120.")
                        
-            elif int(edad) < 18:
-                    diferencia = 18 - int(edad)
-                    print(f"Error: Debes ser mayor de edad para registrarte. te faltan {diferencia} años")
-            elif int(edad) == 18:
-                    print(f"VALIDADO: {edad} apenas llegaste a la mayoria de edad")
-                    break
-            else:# Si no hay letras, la edad  es válido
-                    diferencia= int(edad) - 18 
-                    print(f"VALIDADO: {edad} eres mayor de edad superaste la mayoria de edad por {diferencia} años")
-                    break
+                elif int(edad) < 18:
+                        diferencia = 18 - int(edad)
+                        print(f"Error: Debes ser mayor de edad para registrarte. te faltan {diferencia} años")
+                elif int(edad) == 18:
+                        print(f"VALIDADO: {edad} apenas llegaste a la mayoria de edad")
+                        break
+                else:# Si no hay letras, la edad  es válido
+                        diferencia= int(edad) - 18 
+                        print(f"VALIDADO: {edad} eres mayor de edad superaste la mayoria de edad por {diferencia} años")
+                        lista_edad.append(edad)
+                        break
     while True:
         
-            email.append(input("Por favor, ingresa tu email (gmail, hotmail, yahoo): "))
+            email = input("Por favor, ingresa tu email (gmail, hotmail, yahoo): ")
 
                 # verificar si hay '@' en la cadena
             if "@" not in email or "." not in email:
@@ -82,27 +89,19 @@ while  x == 1:
                         despues_arroba = partes[1]
                         if len(antes_arroba) > 2 and any(despues_arroba.startswith(dominio) for dominio in ['gmail', 'hotmail', 'yahoo']):
                             print("Email aceptado.")
+                            lista_email.append(email)
                             break
                         else:
                             print("DENEGADO: formato de email incorrecto intenta con .com, .com.mx o .mx.")
                     else:
                         print("DENEGADO: ya deberias ser capaz de hacer un email estudie. intente con .com, .com.mx o .mx.")
                         
-
+    print(f"usuario: {nombre } {apellido}")
+    print(f"Edad: {edad}")
+    print(f"Email: {email}")
     x=int(input("DESEA INSERTAR DATOS? 1 PARA SI, 2 PARA NO"))
-    
-
-while True:
-    val_verusuario = input("DESEA VER USUARIOS? SI o NO ")
-    if val_verusuario.lower()=="si":
-        usuario_NR=int(input("que usuario desdea mostrar?"))
-        usuario_NR=usuario_NR-1
-        usuario_NR = int(input("ingresa el numero de usuario que deseas mostrar: "))
-        print(f"usuario: {nombre[usuario_NR]} {apellido[usuario_NR]}")
-        print(f"Edad: {edad[usuario_NR]}")
-        print(f"Email: {email[usuario_NR]}")
-    elif val_verusuario.lower()=="no":
-        print("gracias pero pa cuando la salida al ramen")
-        break
-    else:
-        print("respuesta no valida, por favor ingresa SI o NO")
+print("ahora a la segunda parte de la tarea")
+print("Nombres ingresados:", lista_nombre)
+print("Apellidos ingresados:", lista_apellido)
+print("Emails ingresados:", lista_email)
+print("Edades ingresadas:", lista_edad)
